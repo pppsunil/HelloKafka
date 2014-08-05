79484c2092815cddc8242d62bb7b77bb3ad922ae
pppsunil@gmail.com <myhome1a>
2014-08-05 17:49:52 -0400

    Initial commit

---
+++ src/main/java/com/spnotes/kafka/HelloKafkaConsumer.java
@@ -0,0 +1,64 @@
+package com.spnotes.kafka;
+
+import kafka.api.FetchRequest;
+import kafka.api.FetchRequestBuilder;
+import kafka.api.FetchResponse;
+import kafka.consumer.Consumer;
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaStream;
+import kafka.javaapi.consumer.ConsumerConnector;
+import kafka.javaapi.consumer.SimpleConsumer;
+
+import java.nio.ByteBuffer;
+import java.util.*;
+
+import kafka.javaapi.message.ByteBufferMessageSet;
+import kafka.message.MessageAndOffset;
+import java.io.UnsupportedEncodingException;
+
+
+/**
+ * Created by user on 8/4/14.
+ */
+public class HelloKafkaConsumer extends  Thread {
+    final static String clientId = "SimpleConsumerDemoClient";
+    final static String TOPIC = "pythontest";
+    ConsumerConnector consumerConnector;
+
+
+    public static void main(String[] argv) throws UnsupportedEncodingException {
+        HelloKafkaConsumer helloKafkaConsumer = new HelloKafkaConsumer();
+        helloKafkaConsumer.start();
+
+    }
+
+    public HelloKafkaConsumer(){
+        Properties properties = new Properties();
+        properties.put("zookeeper.connect","localhost:2181");
+        properties.put("group.id","test-group");
+        ConsumerConfig consumerConfig = new ConsumerConfig(properties);
+        consumerConnector = Consumer.createJavaConsumerConnector(consumerConfig);
+    }
+
+    @Override
+    public void run() {
+        Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+        topicCountMap.put(TOPIC, new Integer(1));
+        Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumerConnector.createMessageStreams(topicCountMap);
+        KafkaStream<byte[], byte[]> stream =  consumerMap.get(TOPIC).get(0);
+        ConsumerIterator<byte[], byte[]> it = stream.iterator();
+        while(it.hasNext())
+            System.out.println(new String(it.next().message()));
+
+    }
+
+    private static void printMessages(ByteBufferMessageSet messageSet) throws UnsupportedEncodingException {
+        for(MessageAndOffset messageAndOffset: messageSet) {
+            ByteBuffer payload = messageAndOffset.message().payload();
+            byte[] bytes = new byte[payload.limit()];
+            payload.get(bytes);
+            System.out.println(new String(bytes, "UTF-8"));
+        }
+    }
+}
